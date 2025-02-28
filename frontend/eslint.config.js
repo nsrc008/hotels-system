0712dc7e78cab357b69f845ad5e14c5d0be1734b
nsrc008 <nsrc008@gmail.com>
2025-02-28 17:31:51 -0500

    Asegurar excepción de no-require-imports para jest.setup.ts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import js from '@eslint/js';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import jestPlugin from 'eslint-plugin-jest';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        node: true,
+        jest: true,
+      },
+      parser: typescriptParser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: reactPlugin,
+      '@typescript-eslint': typescriptPlugin,
+      jest: jestPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...typescriptPlugin.configs.recommended.rules,
+      ...jestPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-require-imports': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'no-undef': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+  {
+    files: ['jest.setup.ts'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off', // Excepción confirmada
+    },
+  },
+];
