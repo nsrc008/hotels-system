eae76347303901059127762653851538a1ae29f3
nsrc008 <nsrc008@gmail.com>
2025-02-28 23:20:31 -0500

    config cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://hotel-frontend.onrender.com', 'http://localhost:5173'],
+    'allowed_origins' => ['https://hotel-frontend-8xb9.onrender.com/', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
